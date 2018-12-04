46430b02bb55944003138d5942503a91a977f63d
Massimo Dong <dongmassimo@gmail.com>
2018-12-04 15:49:45 +0800

    serial.c

--- scripts/serial/serial.c
+++ scripts/serial/serial.c
@@ -43,7 +43,7 @@ int main(int argc, char *argv[]){
 
 	int cs = checksum,
 	    cx = checkxor;
-	printf("checksum: %d\n checkxor: %d\n", cs, cx);
+	printf("checksum: %x\ncheckxor: %x\n", cs, cx);
 
 	close(fd);
 }
