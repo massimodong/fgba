163987e9bdb316e43c8b24cbdb63d9e6f7b97c17
Massimo Dong <dongmassimo@gmail.com>
2018-12-08 17:01:20 +0800

    ++

--- tests/nemu-cputests/string/test.c
+++ tests/nemu-cputests/string/test.c
@@ -1,4 +1,5 @@
 #include "trap.h"
+#include<string.h>
 
 char *s[] = {
 	"aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa", 
@@ -13,23 +14,23 @@ char str1[] = "Hello";
 char str[20];
 
 int main() {
-	nemu_assert(_strcmp(s[0], s[2]) == 0);
+	nemu_assert(strcmp(s[0], s[2]) == 0);
 	//_printf("step 1\n");
-	nemu_assert(_strcmp(s[0], s[1]) == -1);
+	nemu_assert(strcmp(s[0], s[1]) == -1);
 	//_printf("%d\n", _strcmp(s[0], s[1]));
 	//_printf("step 2\n");
 	//_printf("%d\n", _strcmp(s[0] + 1, s[1] + 1));
-	nemu_assert(_strcmp(s[0] + 1, s[1] + 1) == -1);
+	nemu_assert(strcmp(s[0] + 1, s[1] + 1) == -1);
 	//_printf("step 3\n");
-	nemu_assert(_strcmp(s[0] + 2, s[1] + 2) == -1);
+	nemu_assert(strcmp(s[0] + 2, s[1] + 2) == -1);
 	//_printf("step 4\n");
-	nemu_assert(_strcmp(s[0] + 3, s[1] + 3) == -1);
+	nemu_assert(strcmp(s[0] + 3, s[1] + 3) == -1);
 	//_printf("step 5\n");
 
-	nemu_assert(_strcmp( _strcat(_strcpy(str, str1), s[3]), s[4]) == 0);
+	nemu_assert(strcmp( strcat(strcpy(str, str1), s[3]), s[4]) == 0);
 	//_printf("step 6\n");
 
-	nemu_assert(_memcmp(_memset(str, '#', 5), s[5], 5) == 0);
+	nemu_assert(memcmp(memset(str, '#', 5), s[5], 5) == 0);
 	//_printf("step 7\n");
 
 	_halt(0);
