4e757c9ad7418cc5e01d149b60904a809ab6f4eb
Fancy <peifancy@gmail.com>
2018-12-21 20:11:13 +0800

    VGA add offset

--- quartus_proj/graphic.v
+++ quartus_proj/graphic.v
@@ -22,13 +22,14 @@ module graphic(
 wire [8:0]row_addr;
 wire [9:0]col_addr;
 wire rdn;
-wire inframe = (row_addr < 160) && (col_addr < 240);
+wire inframe = (row_addr < 160) && (col_addr - offset < 240);
 
 wire mode3 = dispcnt[2:0] == 3'h3;
 wire mode4 = dispcnt[2:0] == 3'h4;
+wire [2:0]offset = mode3 ? 2 : 4;
 
 always @(*) begin
-	if(inframe) begin
+	if((row_addr < 160) && (col_addr < 240)) begin
 		case (1'b1)
 			mode3: vram_addr = {row_addr[7:0], 8'b0} - {row_addr, 4'b0} + col_addr;
 			mode4: vram_addr = {row_addr, 7'b0} - {row_addr, 3'b0} + col_addr[9:1] + (dispcnt[4] ? 16'h5000: 16'b0);
@@ -39,16 +40,9 @@ end
 
 assign palette_addr = col_addr[0] ? vram_data[15:8] : vram_data[7:0]; //color[vram[row * 120 + col / 2][15:8 or 7:0]]
 
-reg [3:0]delay = 4'h0;
-always @(posedge clk) begin
-	delay <= {delay[2:0], inframe};
-end
-
-wire showColor = mode3 ? delay[1] : delay[3];
-wire [14:0]color = showColor ? (mode3 ? vram_data[14:0] : palette_data[14:0]) : 15'b110111101111011;
+wire [14:0]color = inframe ? (mode3 ? vram_data[14:0] : palette_data[14:0]) : 15'b110111101111011; //grey if out of frame
 
 vgac vga1(
-	//.d_in(inframe ? last_data : 15'b110111101111011), //grey if out of frame
 	.d_in(color),
 	.vga_clk(clk),
 	.clrn(1'b1),
@@ -59,7 +53,8 @@ vgac vga1(
 	.b(B[7:3]),
 	.rdn(rdn),
 	.hs(HS),
-	.vs(VS)
+	.vs(VS),
+	.offset(offset)
 );
 
 assign R[2:0] = 3'h0;
--- quartus_proj/vgac.v
+++ quartus_proj/vgac.v
@@ -3,7 +3,7 @@
   Computer Principles and Design in Verilog HDL
   by Yamin Li, published by A JOHN WILEY & SONS
 ************************************************/
-module vgac (vga_clk,clrn,d_in,row_addr,col_addr,rdn,r,g,b,hs,vs);
+module vgac (vga_clk,clrn,d_in,row_addr,col_addr,rdn,r,g,b,hs,vs,offset);
     input     [14:0] d_in;     // rrrrr_ggggg_bbbbb, pixel
     input            vga_clk;  // 25MHz
     input            clrn;
@@ -12,7 +12,8 @@ module vgac (vga_clk,clrn,d_in,row_addr,col_addr,rdn,r,g,b,hs,vs);
     output reg [4:0] r,g,b;    // red, green, blue colors, 5-bit for each
     output reg       rdn;      // read pixel RAM (active low)
     output reg       hs,vs;    // horizontal and vertical synchronization
-    // h_count: vga horizontal counter (0-799 pixels)
+    input      [2:0] offset;
+	 // h_count: vga horizontal counter (0-799 pixels)
     reg [9:0] h_count = 10'd0;
     always @ (posedge vga_clk or negedge clrn) begin
         if (!clrn) begin
@@ -38,7 +39,7 @@ module vgac (vga_clk,clrn,d_in,row_addr,col_addr,rdn,r,g,b,hs,vs);
     end
     // signals, will be latched for outputs
     wire [9:0] row    =  v_count - 10'd35;     // ( 2+33= 35) pixel ram row address 
-    wire [9:0] col    =  h_count - 10'd143;    // (96+48=144) pixel ram col address 
+    wire [9:0] col    =  h_count + offset - 10'd143;    // (96+48=144) pixel ram col address 
     wire       h_sync = (h_count > 10'd95);    //  96 -> 799
     wire       v_sync = (v_count > 10'd1);     //   2 -> 524
     wire       read   = (h_count > 10'd142) && // 143 -> 782 =
